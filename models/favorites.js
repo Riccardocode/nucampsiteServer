e5e3414e0dfa28269ab4ddda04f2737e2b78152b
°Riccardo <52591461+Riccardocode@users.noreply.github.com>
2023-05-20 17:29:51 +0200

    Week4 assignment fixed

--- models/favorites.js
+++ models/favorites.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema; 
 
 const favoriteSchema = new Schema({
-    user:{
-        type: Schema.Types.ObjectId,
+    user: {
+        type: Schema.Types.ObjectId, 
         ref: 'User'
-    },
-    campsites:{
-        type: Schema.Types.ObjectId,
+    }, 
+    campsites: [{
+        type: Schema.Types.ObjectId, 
         ref: 'Campsite'
-    }
+    }]
 }, {
-    timestamps:true
+    timestamps: true
 })
 
-module.exports = mongoose.model('Favorite',favoriteSchema);
+module.exports = mongoose.model('Favorite', favoriteSchema);
--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -1,38 +1,35 @@
 const express = require('express');
-const favoriteRouter = express.Router();
-const authenticate = require('../authenticate');
-const Favorite = require('../models/favorite');
-const cors = require('./cors');
+const favoriteRouter = express.Router(); 
+const authenticate = require('../authenticate'); 
+const Favorite = require('../models/favorites'); 
+const cors = require('./cors'); 
 
 
 
 favoriteRouter.route('/')
-.options(cors.corsWithOptions,(req,res) =>res.sendStatus(200))
-.get(cors.cors,authenticate.verifyUser,(req,res,next) =>{
-    Favorite.find({user:req.user_id})
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+    Favorite.find({ user: req.user._id })
     .populate('user')
     .populate('campsites')
     .then(favorite => res.status(200).json(favorite))
     .catch(err => next(err))
 })
-.post(cors.corsWithOptions,authenticate.verifyUser,(req,res,next) =>{
-    Favorite.findOne({user:req.user_id})
-    .then(favorite =>{
-        if(favorite){
-            req.body.forEach(fav =>{
-                if(!favorite.campsites.includes(fav._id)){
+.post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+    .then(favorite => {
+        if (favorite) {
+            req.body.forEach(fav => {
+                if (!favorite.campsites.includes(fav._id)) {
                     favorite.campsites.push(fav._id)
                 }
-
             })
             favorite.save()
             .then(favorite => res.status(200).json(favorite))
             .catch(err => next(err))
-        }else{
-            Favorite.create({user:req.user._id})
-            .then(favorite =>{
-                req.body.forEach(fav =>favorite.campsites.push(fav._id))
-            })
+        } else {
+            Favorite.create({ user: req.user._id })
+            .then(favorite => req.body.forEach(fav => favorite.campsites.push(fav._id)))
             .catch(err => next(err))
 
             favorite.save()
@@ -40,57 +37,55 @@ favoriteRouter.route('/')
             .catch(err => next(err))
         }
     })
-    .catch(err =>next(err))
+    .catch(err => next(err))
 })
-.delete(cors.corsWithOptions, authenticate.verityUser, (req,res,next) =>{
-    Favorite.findOneAndDelete({user:req.user._id})
-    .then(favorite =>{
-        if(favorite){
+.delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOneAndDelete({ user: req.user._id })
+    .then(favorite => {
+        if (favorite) {
             res.status(200).json(favorite)
-        }else{
-            res.status(200).end('You do not have any favoirtes to delete!')
+        } else {
+            res.status(200).end('You do not have any favorites to delete!')
         }
     })
     .catch(err => next(err))
 })
 
 favoriteRouter.route('/:campsiteId')
-.options(cors.corsWithOptions,(req,res) =>res.sendStatus(200))
-.post(cors.corsWithOptions,authenticate.verifyUser, (req,res,next) =>{
-    Favorite.findOne({user:req.user._id})
-    .then(favorite =>{
-        if(favorite){
-            if(!favorite.campsites.include(req.params.campsiteId)){
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+    .then(favorite => {
+        if (favorite) {
+            if (!favorite.campsites.includes(req.params.campsiteId)) {
                 favorite.campsites.push(req.params.campsiteId);
+
                 favorite.save()
                 .then(favorite => res.status(200).json(favorite))
                 .catch(err => next(err))
-            }else{
-                res.status(200).end('Taht campsite is already a favorite!');
+            } else {
+                res.status(200).end('That campsite is already a favorite!');
             }
-        }else{
-            Favorite.create({user:req.user._id, campsites:[req.params.campsiteId]})
+        } else {
+            Favorite.create({ user: req.user._id, campsites: [req.params.campsiteId] })
             .then(favorite => res.status(200).json(favorite))
             .catch(err => next(err))
         }
     })
-    .catch(err =>next(err))
+    .catch(err => next(err))
 })
-.delete(cors.corsWithOptions,authenticate.verifyUser, (req,res,next) =>{
-    Favorite.findOne({user:req.user._id})
-    .then(favorite =>{
-        if(favorite){
-            const index = favorite.campsites.indexOf(req.params.campsiteId);
+.delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+    .then(favorite => {
+        if (favorite) {
+            const index = favorite.campsites.indexOf(req.params.campsiteId); 
 
-            if(index>= 0){
-                favorite.campsites.splice(index,1)
-                favorite.save()
-                .then(favorite => res.status(200).json(favorite))
-                .catch(err => next(err))
-            }else{
-                res.status(200).end('You do not have any favorites to delete!');
-            }
-        }else{
+            if (index >= 0) favorite.campsites.splice(index, 1); 
+
+            favorite.save()
+            .then(favorite => res.status(200).json(favorite))
+            .catch(err => next(err))
+        } else {
             res.status(200).end('You do not have any favorites to delete!')
         }
     })
